abfaf6e4f01ac0090d7e53499fa6d3264d2b7f6f
Esteban Arce <estebanarce1500@gmail.com>
2023-07-06 22:53:35 -0600

    java
    creacion del archivo en Netbeans

---
+++ ProyectoBD/src/main/java/com/mycompany/proyectobd/ProyectoBD.java
@@ -0,0 +1,16 @@
+/*
+ * Aprendiendo a usar java- Proyecto BD
+ */
+
+package com.mycompany.proyectobd;
+
+/**
+ *
+ * @author Esteban
+ */
+public class ProyectoBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
