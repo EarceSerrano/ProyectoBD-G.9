1d0e43610fe293778a55477bc0e7cdabfe17459e
Esteban Arce <estebanarce1500@gmail.com>
2023-07-07 23:00:27 -0600

    cambio
    Se tuvo que hacer un proyecto nuevo, esto debido a que el formato que dio netbeans no poseia la carpeta "Libraries", indispensable para la conexion con la base de datos

---
+++ ProyectoDB/src/main/java/ConexionDB/conexionOracle.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConexionDB;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Esteban
+ */
+public class conexionOracle {
+    private Connection conn = null;
+    private String url, user, pass;
+    
+    public ConexionOracle(){
+        conectar();
+    }
+ 
+    private void conectar(){
+        try{  
+            Class.forName("oracle.jdbc.OracleDriver") ;//Driver DB
+            url = "jdbc:oracle:thin:@localhost:1521:orcl" ;
+            user= "JAVA";
+            pass= "JAVA";
+                    conn = DriverManager.getConnection(url, user, pass);
+                    System.out.println("Conectando");
+                    
+        } catch(Exception e){
+            System.out.println("No se pudo establecer la conexion");
+        }
+    }
+    
+    
+    public void desconectar(){
+        try{
+            conn.close();
+        } catch(Exception e){
+            System.out.println("Error, no se pudo desconectar ");
+        }
+    }
+    
+}
