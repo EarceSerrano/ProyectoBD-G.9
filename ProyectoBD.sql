813685c9d73d83496dbab068429596ce6790314b
Esteban Arce <estebanarce1500@gmail.com>
2023-07-14 22:12:41 -0600

    Update ProyectoBD.sql

--- ProyectoBD.sql
+++ ProyectoBD.sql
@@ -25,7 +25,7 @@ CREATE TABLE tb_clientes(
 telefono     NUMBER,
 nombre    VARCHAR2(100),
 ID        VARCHAR2(50),
-correo    VARCHAR2(50)
+correo    VARCHAR2(50) 
 );
 
 CREATE TABLE tb_facturas(
@@ -48,5 +48,22 @@ ID_categorias VARCHAR2(50)
 
 commit;
 
-SELECT * FROM tb_proveedores;
+---Inserccion de algunos datos en las tablas (para trabajar)
 
+---) insert en tb_proveedores
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(234312253, 'info@textitech.com', 'PRO001', 'TextiTech');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2902, 'contact@fiberfab.com', 'PRO002', 'FiberFab');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'sales@stylesource.com', 'PRO003', 'StyleSource');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'info@stitchmaster.com', 'PRO004', 'StitchMaster');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'inquiries@fabrico.com', 'PRO005', 'FabriCo');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'contact@threadtech.com', 'PRO006', 'ThreadTech');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'info@fashionfusion.com', 'PRO007', 'FashionFusion');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'sales@clothcraft.com', 'PRO008', 'ClothCraft');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'info@weaveworx.com', 'PRO009', 'WeaveWorx');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'contact@trimtrend.com', 'PRO010', 'TrimTrend');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'info@fabriclink.com', 'PRO011', 'FabricLink');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'sales@designdrape.com', 'PRO012', 'DesignDrape');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'contact@threadworks.com', 'PRO013', 'ThreadWorks');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'info@sewingsupplies.com', 'PRO014', 'SewingSupplies');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'inquiries@trendytextiles.com', 'PRO015', 'TrendyTextiles');
+INSERT INTO tb_proveedores(numero, correo, ID, nombre) VALUES(2, 'sales@materialmakers.com', 'PRO016', 'MaterialMakers');
