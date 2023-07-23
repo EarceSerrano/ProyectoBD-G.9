5dd375a8568a59f7ba3af46bc880abec51b0ef54
ky <100336613+kioryzou@users.noreply.github.com>
2023-07-22 21:48:35 -0600

    triggers que se crean correctamente
    Estos son los 8 triggers que probé que se crearan correctamente, para futuras vistas, deben de saber que use como guía el código de la Branch main, el 7/22/23

--- ProyectoBD.sql
+++ ProyectoBD.sql
@@ -114,3 +114,90 @@ BEGIN
   
   RETURN 0; --valor ejemplo
 END;
+
+
+
+
+---TRIGGER COMPRABDOS DE QUE SE CREAN CORRECTAMENTE---------------
+
+-- Trigger1
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio
+BEFORE INSERT OR UPDATE ON tb_productos
+FOR EACH ROW
+BEGIN
+  IF :new.nom_producto IS NULL OR :new.precio IS NULL OR :new.ID_categoria IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20001, 'No se permiten campos vacíos en tb_productos');
+  END IF;
+END;
+
+
+
+-- Trigger2
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio_proveedores
+BEFORE INSERT OR UPDATE ON tb_proveedores
+FOR EACH ROW
+BEGIN
+  IF :new.nombre_p IS NULL OR :new.numero IS NULL OR :new.correo IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20002, 'No se permiten campos vacíos en tb_proveedores');
+  END IF;
+END;
+
+-- Trigger3
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio_materiales
+BEFORE INSERT OR UPDATE ON tb_materiales
+FOR EACH ROW
+BEGIN
+  IF :new.unidadPrecio IS NULL OR :new.ID_proveedor IS NULL OR :new.unidades IS NULL OR :new.descripcion IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20003, 'No se permiten campos vacíos en tb_materiales');
+  END IF;
+END;
+
+-- Trigger4
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio_categorias
+BEFORE INSERT OR UPDATE ON tb_categorias
+FOR EACH ROW
+BEGIN
+  IF :new.nombre_categoria IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20004, 'No se permite campo vacío en tb_categorias');
+  END IF;
+END;
+
+-- Trigger5
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio_empleados
+BEFORE INSERT OR UPDATE ON tb_empleados
+FOR EACH ROW
+BEGIN
+  IF :new.nombre IS NULL OR :new.apellido IS NULL OR :new.ID_puesto IS NULL OR :new.fecha_nacimiento IS NULL OR :new.numero_ventas IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20005, 'No se permiten campos vacíos en tb_empleados');
+  END IF;
+END;
+
+-- Trigger6
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio_factura
+BEFORE INSERT OR UPDATE ON tb_factura
+FOR EACH ROW
+BEGIN
+  IF :new.ID_cliente IS NULL OR :new.ID_empleado IS NULL OR :new.correo IS NULL OR :new.fecha IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20006, 'No se permiten campos vacíos en tb_factura');
+  END IF;
+END;
+
+-- Trigger7
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio_detalles_factura
+BEFORE INSERT OR UPDATE ON detalles_factura
+FOR EACH ROW
+BEGIN
+  IF :new.ID_factura IS NULL OR :new.ID_producto IS NULL OR :new.cantidad IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20007, 'No se permiten campos vacíos en detalles_factura');
+  END IF;
+END;
+
+-- Trigger8
+CREATE OR REPLACE TRIGGER trigger_alerta_campo_vacio_tb_facturas
+BEFORE INSERT OR UPDATE ON tb_facturas
+FOR EACH ROW
+BEGIN
+  IF :new.ID_cliente IS NULL OR :new.ID_empleado IS NULL OR :new.correo IS NULL OR :new.fecha IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20008, 'No se permiten campos vacíos en tb_facturas');
+  END IF;
+END;
